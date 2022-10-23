195f0f61f88f9cc2bdcea521cfa8aaad4eeae9b8
tibs <git@tibinonest.me>
2022-10-22 23:37:31 -0400

    QOL changes also CNAME

--- _config.ts
+++ _config.ts
@@ -1,12 +1,23 @@
 import lume from "lume/mod.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({
-    src: "./src",
+	src: "./src",
+	prettyUrls: "no-html-extension"
 });
 
-site.copy("assets");
-
 site.use(code_highlight());
+site.use(lightningCss());
+site.use(minifyHTML({
+	options: {
+		do_not_minify_doctype: true,
+		ensure_spec_compliant_unquoted_attribute_values: true,
+		keep_spaces_between_attributes: true
+	}
+}));
+
+site.copy("static", ".");
 
 export default site;
