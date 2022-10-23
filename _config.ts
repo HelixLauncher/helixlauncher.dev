6759122ca353579123d516a078499bdd1dab6938
tibs <git@tibinonest.me>
2022-10-23 18:20:01 -0400

    Some stylistic changes

--- _config.ts
+++ _config.ts
@@ -1,14 +1,13 @@
 import lume from "lume/mod.ts";
-import code_highlight from "lume/plugins/code_highlight.ts";
+import sass from "lume/plugins/sass.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({
-	src: "./src",
-	prettyUrls: "no-html-extension"
+	src: "./src"
 });
 
-site.use(code_highlight());
+site.use(sass());
 site.use(lightningCss());
 site.use(minifyHTML({
 	options: {
